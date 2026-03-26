9e03827a5130e7d96cdc31a1c607ba003984462f
alameri-ui <ye734999901@gmail.com>
2026-03-27 02:17:46 +0300

    Update usdt-erc20-split.php file with new plugin information

--- wp-content/plugins/usdt-erc20-split/usdt-erc20-split.php
+++ wp-content/plugins/usdt-erc20-split/usdt-erc20-split.php
@@ -1,9 +1,25 @@
 <?php
 /**
- * Plugin Name: USDT ERC20 Split
- * Description: A simple WordPress plugin that splits payments between multiple wallets.
+ * Plugin Name: USDT ERC20 Split (WooCommerce)
+ * Plugin URI: https://github.com/alameri-ui/usdt-erc20-split
+ * Description: WooCommerce USDT (ERC20) split payment gateway on Ethereum mainnet (99% to merchant + 1% platform fee).
  * Version: 1.0.0
- * Author: Your Name
+ * Author: Zaid Alameri Digital
+ * Author URI: https://github.com/alameri-ui
+ * License: GPLv2 or later
+ * License URI: https://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain: usdt-erc20-split
+ * Requires PHP: 7.4
+ * WC requires at least: 6.0
+ * WC tested up to: 9.0
  */
 
-// Main plugin file logic here
+if (!defined('ABSPATH')) {
+  exit;
+}
+
+define('USDT_ERC20_SPLIT_VERSION', '1.0.0');
+define('USDT_ERC20_SPLIT_PLUGIN_FILE', __FILE__);
+define('USDT_ERC20_SPLIT_DIR', plugin_dir_path(__FILE__));
+
+action_hooks:;
