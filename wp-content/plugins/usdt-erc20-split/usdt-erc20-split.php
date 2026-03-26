a46eea751bb9bab58ae1ca5c5b4737e4e484a0c0
alameri-ui <ye734999901@gmail.com>
2026-03-27 02:12:56 +0300

    Fix plugin file structure for WordPress

---
+++ wp-content/plugins/usdt-erc20-split/usdt-erc20-split.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Plugin Name: USDT ERC20 Split
+ * Description: A simple WordPress plugin that splits payments between multiple wallets.
+ * Version: 1.0.0
+ * Author: Your Name
+ */
+
+// Main plugin file logic here
